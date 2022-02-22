94f3bd3a886acc8818cc99ec2f247908a61199a4
sowjanyareddy1 <sowjanya.reddy6@gmail.com>
2022-02-22 12:36:25 +0530

    Create Stage1.py

---
+++ Webscrapper/Stage1.py
@@ -0,0 +1,14 @@
+# load the necessary libraries
+import requests, json
+url = input()
+r = requests.get(url)
+# check if the page is loading or not 200 means page loading
+if r.status_code == 200:
+    try:
+      # use json to get the data of the page
+        r.json()
+        print(r.json()['content'])
+    except (KeyError, json.decoder.JSONDecodeError):
+        print('\nInvalid quote resource!')
+else:
+    print('\nInvalid quote resource!')
